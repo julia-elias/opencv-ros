8534ec3573616b50ce6b5dffe5ab102aea252a62
Julia Elias <jjelias03@gmail.com>
2022-03-01 18:46:37 -0600

    first commit

---
+++ build/CMakeFiles/cvexample.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fri/Downloads/opencv_hw_template/ColorFilter.cpp" "/home/fri/Downloads/opencv_hw_template/build/CMakeFiles/cvexample.dir/ColorFilter.cpp.o"
+  "/home/fri/Downloads/opencv_hw_template/ReadAVI.cpp" "/home/fri/Downloads/opencv_hw_template/build/CMakeFiles/cvexample.dir/ReadAVI.cpp.o"
+  "/home/fri/Downloads/opencv_hw_template/cvexample.cpp" "/home/fri/Downloads/opencv_hw_template/build/CMakeFiles/cvexample.dir/cvexample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/ros/melodic/include/opencv-3.3.1-dev"
+  "/opt/ros/melodic/include/opencv-3.3.1-dev/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
